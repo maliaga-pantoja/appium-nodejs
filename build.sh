31782ce9fc1536d04b7a62aee09801e221bb552e
Miguel Aliaga <maliaga.pantoja@gmail.com>
2021-09-22 00:42:14 -0500

    adding basics tests

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 rm -rf bundle.zip webdriverio-sample-appium-tests-1.0.0.tgz
 podman run --rm -v $PWD:/app -w /app node npm i
 podman run --rm -v $PWD:/app -w /app node npm run bundle
-zip -r9 bundle.zip webdriverio-sample-appium-tests-1.0.0.tgz
+./node_modules/.bin/npm-bundle bundle.tgz
+zip -r9 bundle.zip bundle.tgz
--- tests/android-web-session.test.js
+++ tests/android-web-session.test.js
@@ -3,22 +3,22 @@ const androidOptions = require('./helpers/caps').opts;
 const assert = require('chai').assert;
 
 describe('Create Chrome web session', function () {
-  let client;
+  let browser;
 
   before(async function () {
-    client = await webdriverio.remote(androidOptions);
+    browser = await webdriverio.remote(androidOptions);
   });
 
   after(async function () {
-    return await client.deleteSession();
+    return await browser.deleteSession();
   });
 
   it('should create and destroy Android browser session', async function () {
     // Navigate to google.com
     // const client = await webdriverio.remote(androidOptions);
-    await client.url('https://www.google.com');
-    await client.takeScreenshot();
-    const title = await client.getTitle();
+    await browser.url('https://www.google.com');
+    await browser.saveScreenshot();
+    const title = await browser.getTitle();
     assert.equal(title, 'Google');
   });
 });
